f73fd17f12cdba9b4f638215e51083eab3792939
John Ibsen <jibsen@indigoag.com>
2020-05-16 16:23:01 -0400

    Next BIG batch of titles

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ assert(passportTitleGenerator.seed(123));
 assert(passportTitleGenerator.adjPrime(3));
 assert(passportTitleGenerator.nounPrime(7));
 
-assert.deepEqual(passportTitleGenerator(), [ 'Rowdy', 'Photographer' ]);
+assert.deepEqual(passportTitleGenerator(), [ 'Precocious', 'Other Half' ]);
 
 assert(passportTitleGenerator.seed(456) === false);
 
